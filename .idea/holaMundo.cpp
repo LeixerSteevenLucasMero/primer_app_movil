ca1a3e09e4be620ef22084445f4b7dbb0e76b930
LeixerSteevenLucasMero <79165244+LeixerSteevenLucasMero@users.noreply.github.com>
2022-08-08 21:21:26 -0500

    holajj

---
+++ .idea/holaMundo.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Leixe on 08/08/2022.
+//
+
+#include "holaMundo.h"
---
+++ .idea/holaMundo.h
@@ -0,0 +1,16 @@
+//
+// Created by Leixe on 08/08/2022.
+//
+
+#ifndef PRIMER_APP_MOVIL_HOLAMUNDO_H
+#define PRIMER_APP_MOVIL_HOLAMUNDO_H
+
+
+
+class holaMundo {
+
+};
+
+
+
+#endif //PRIMER_APP_MOVIL_HOLAMUNDO_H
